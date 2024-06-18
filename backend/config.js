ec3aacb894c889231216765c4c6ba9037a8d9fbf
ShubhamJR8 <shubjr1998@gmail.com>
2024-06-19 02:17:20 +0530

    MERN CRUD RestApi's

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://shubhaku98:qgbOJvPwy2szgzSU@books-store-mern.ixmongq.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
